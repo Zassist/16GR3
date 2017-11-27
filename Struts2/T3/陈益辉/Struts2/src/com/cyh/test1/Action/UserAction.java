db36f5644012b5dcea997669736309f9754b2ed9
1640506509 <1640506509@qq.com>
2017-11-27 15:48:54 +0800

    1.陈益辉Struts2课程T3的作业

---
+++ Struts2/T3/陈益辉/Struts2/src/com/cyh/test1/Action/UserAction.java
@@ -0,0 +1,19 @@
+package com.cyh.test1.Action;
+
+import com.cyh.test1.bean.User;
+import com.opensymphony.xwork2.Action;
+
+public class UserAction implements Action {
+	private User user;
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return Action.SUCCESS;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+}
