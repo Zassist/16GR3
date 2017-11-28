7aa3830a5eefc7ab3e048d4b1f204066aafd1908
WOXR <602762625@qq.com>
2017-11-28 21:16:48 +0800

    1，任新杰提交Struts2T03上机作业

---
+++ Struts2/T3/StrutsT3/src/com/qhit/rxj/bean/UserInfo.java
@@ -0,0 +1,15 @@
+package com.qhit.rxj.bean;
+
+import java.util.Date;
+
+public class UserInfo {
+	private int id;				//用户id
+	private String name;		//用户名
+	private String password;	//密码
+	
+	private Date brithday;		//出生日期
+	private String sex;			//性别
+	private boolean action;		//是否启用
+	
+	
+}
